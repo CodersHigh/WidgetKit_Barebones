c4673d80f4d287a137b0dabb89c1189293ff2c29
이로운 <lru040409@gmail.com>
2022-07-20 15:35:30 +0900

    UI 세팅

--- WidgetKit_Barebones/ViewController.swift
+++ WidgetKit_Barebones/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+     
+    private let field: UITextField = {
+        let field = UITextField()
+        field.placeholder = "위젯에 어떤 텍스트를 띄울까요?"
+        field.backgroundColor = .white
+        field.borderStyle = .roundedRect
+        return field
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("적용", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 12
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        view.addSubview(field)
+        view.addSubview(button)
+        field.becomeFirstResponder()
+        
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        field.frame = CGRect(x: 40, y: view.safeAreaInsets.top+40, width: view.frame.size.width-80, height: 45)
+        button.frame = CGRect(x: 40, y: view.safeAreaInsets.top+100, width: view.frame.size.width-80, height: 45)
+    }
+    
+    @objc private func didTapButton() {
+        
+    }
 
 }
 
