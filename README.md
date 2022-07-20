696ce11a2e1e594153caae5ed325cfe57faf7e99
이로운 <lru040409@gmail.com>
2022-07-20 14:31:14 +0900

    WidgetKit 플젝 파일 셋업

---
+++ README.md
@@ -0,0 +1,2 @@
+# WidgetKit_Barebones
+WidgetKit의 기본적인 기능 구현을 익히는 데에 도움을 주는 Bare-bones 프로젝트
---
+++ WidgetKit_Barebones/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetKit_Barebones
+//
+//  Created by 이로운 on 2022/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
